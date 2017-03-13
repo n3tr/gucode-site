8c9497c1405ddef4e700b5968efad9e3811b48aa
Jirat K <saakyz@gmail.com>
2017-03-13 22:03:39 +0700

    use https for giphy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=godfather')
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=godfather')
     .then( res => res.json() )
     .then( json => json.data.image_url )
     .then( (imageUrl) => {
